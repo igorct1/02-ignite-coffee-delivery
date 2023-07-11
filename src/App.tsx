87252060a5b0e39955ea81f03f42e730cf74ca1d
igoreason <igorct77@gmail.com>
2023-07-11 19:48:11 -0300

    feat: adding router to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './styles/global.css'
 import { ThemeProvider } from 'styled-components'
 import { DefaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={DefaultTheme}>
-      <div>Home</div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
