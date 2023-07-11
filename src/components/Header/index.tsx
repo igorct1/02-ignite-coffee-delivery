6a9f8233ea4cf3622757a9af7536286d6aef93bd
igoreason <igorct77@gmail.com>
2023-07-11 20:03:34 -0300

    feat: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,26 @@
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import { Logo } from '../Logo'
+import {
+  HeaderButtonCard,
+  HeaderContainer,
+  HeaderInfo,
+  HeaderLocation,
+} from './styles'
+
 export function Header() {
-  return <header>header</header>
+  return (
+    <HeaderContainer>
+      <Logo />
+      <HeaderInfo>
+        <HeaderLocation>
+          <MapPin size={22} weight="fill" />
+          <span>São Paulo, SP</span>
+        </HeaderLocation>
+
+        <HeaderButtonCard>
+          <ShoppingCart size={22} weight="fill" />
+        </HeaderButtonCard>
+      </HeaderInfo>
+    </HeaderContainer>
+  )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,45 @@
+import { styled } from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  padding: 2rem 6rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const HeaderInfo = styled.div`
+  display: flex;
+  gap: 1.2rem;
+  align-items: center;
+`
+
+export const HeaderLocation = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  background: ${(props) => props.theme.colors['purple-light']};
+  color: ${(props) => props.theme.colors['purple-dark']};
+
+  padding: 0.8rem;
+
+  border-radius: 8px;
+
+  span {
+    font-size: ${(props) => props.theme.fontSizes['text-S']};
+  }
+`
+
+export const HeaderButtonCard = styled.button`
+  background: ${(props) => props.theme.colors['yellow-light']};
+  border: 0;
+  padding: 0.8rem;
+
+  border-radius: 8px;
+  line-height: 0;
+
+  color: ${(props) => props.theme.colors['yellow-dark']};
+
+  cursor: pointer;
+`
